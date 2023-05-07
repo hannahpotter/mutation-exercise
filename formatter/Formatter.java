028adaafe10edf1644672e83a2b2b84c972889c6
rjust <rjust.dev@gmail.com>
2023-05-07 12:06:13 -0700

    Incrementally export coverage information.

--- formatter/Formatter.java
+++ formatter/Formatter.java
@@ -1,94 +1,79 @@
 package formatter;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.OutputStream;
-import java.io.PrintStream;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.lang.reflect.Field;
 
 import junit.framework.AssertionFailedError;
 import junit.framework.Test;
-import junit.framework.TestSuite;
 
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.taskdefs.optional.junit.JUnitResultFormatter;
 import org.apache.tools.ant.taskdefs.optional.junit.JUnitTest;
 
+import net.sourceforge.cobertura.coveragedata.CoverageDataFileHandler;
+import net.sourceforge.cobertura.coveragedata.ProjectData;
+import net.sourceforge.cobertura.coveragedata.TouchCollector;
+
 public class Formatter implements JUnitResultFormatter {
-    static int testNo = 1;
+    private int testNo = 1;
 
+    @Override
+    public void startTestSuite(JUnitTest junitTest) throws BuildException { }
 
-	@Override
-	public void endTestSuite(JUnitTest arg0) throws BuildException {
-		File original = new File("cobertura.ser");
- 		File copy = new File("initial-cobertura.ser");
-		try {
-			original.delete();
-        	Files.copy(copy.toPath(), original.toPath());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    @Override
+    public void endTestSuite(JUnitTest arg0) throws BuildException {
+        // No need to export the coverage data again when the JVM exits.
+        // TODO: The thread still runs when the JVM shuts down.
+        ProjectData.turnOffAutoSave();
+    }
 
-	@Override
-	public void setOutput(OutputStream arg0) {
-	}
+    @Override
+    public void startTest(Test test) { }
 
-	@Override
-	public void setSystemError(String arg0) {
-	}
-
-	@Override
-	public void setSystemOutput(String arg0) {
-	}
-
-	@Override
-	public void startTestSuite(JUnitTest junitTest) throws BuildException {
-		File original = new File("cobertura.ser");
- 		File copy = new File("initial-cobertura.ser");
-		try {
-        	Files.copy(original.toPath(), copy.toPath());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-
-	@Override
-	public void addError(Test test, Throwable t) {
-	}
-
-	@Override
-	public void addFailure(Test test, AssertionFailedError t) {
-	}
-
-	@Override
-	public void endTest(Test test) {
-        File original = new File("cobertura.ser");
-        File folder = new File("coverage_original_results/" + testNo);
- 		File new_result = new File("coverage_original_results/" + testNo+ "/cobertura.ser");
-		try {
-			folder.mkdirs();
-        	Files.copy(original.toPath(), new_result.toPath());
-        	original.delete();
-
-			File copy = new File("initial-cobertura.ser");
-			Files.copy(copy.toPath(), original.toPath());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+    @Override
+    public void endTest(Test test) {
+        // No need to create the directories; Cobertura does this during export
+        exportAndResetCoverageData("coverage_original_results/" + testNo+ "/cobertura.ser");
 
         testNo++;
-	}
+    }
 
-	@Override
-	public void startTest(Test test) {
-	}
+    /**
+     * Export and reset the current coverage counters.
+     *
+     * ProjectData.saveGlobalProjectData would be a more convenient way to do
+     * this but it uses a very conservative 1-second sleep to avoid data
+     * corruption. This is unnecessary in our use case.
+     */
+    private void exportAndResetCoverageData(String serFile) {
+        // Get current coverage data and write to disk.
+        ProjectData projectData = ProjectData.getGlobalProjectData();
+        TouchCollector.applyTouchesOnProjectData(projectData);
+        CoverageDataFileHandler.saveCoverageData(projectData, new File(serFile));
+
+        // Reset the global coverage data.
+        try {
+            Field f = ProjectData.class.getDeclaredField("globalProjectData");
+            f.setAccessible(true);
+            f.set(null, new ProjectData());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void addError(Test test, Throwable t) { }
+
+    @Override
+    public void addFailure(Test test, AssertionFailedError t) { }
+
+    @Override
+    public void setOutput(OutputStream arg0) { }
+
+    @Override
+    public void setSystemError(String arg0) { }
+
+    @Override
+    public void setSystemOutput(String arg0) { }
 }
