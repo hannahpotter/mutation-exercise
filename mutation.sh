efd7ac4170e79c4167e2171ad6726d98aa7955c9
rjust <rjust.dev@gmail.com>
2023-05-10 14:38:46 -0700

    Avoid confusion with mutants.log

--- mutation.sh
+++ mutation.sh
@@ -3,6 +3,6 @@
 MAJOR_HOME="lib/major"
 
 $MAJOR_HOME/bin/ant mutation
-echo $(grep ,LIVE mutation_results/killed.csv | cut -f1 -d',') | tr ' ' ','> mutation_results/live_mutants.log
+echo $(grep ,LIVE mutation_results/killed.csv | cut -f1 -d',') | tr ' ' ','> mutation_results/live_mutants.txt
 
 echo Live mutants: $(grep ,LIVE mutation_results/killed.csv | cut -f1 -d',')
