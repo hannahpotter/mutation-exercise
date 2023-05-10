745c91a7008dea33d50f80f8696c909d28cdcabd
rjust <rjust.dev@gmail.com>
2023-05-10 14:12:49 -0700

    Make report generation faster.

--- liv_cov.sh
+++ liv_cov.sh
@@ -2,9 +2,9 @@
 
 MAJOR_HOME="lib/major"
 
-# Run mutation testing
-$MAJOR_HOME/bin/ant mutation
-echo Live mutants: $(grep ,LIVE mutation_results/killed.csv | cut -f1 -d',')
+echo RUNNING MUTATION ANALYSIS...
+./mutation.sh > mutant_report/report.log
+grep "Live mutants:" mutant_report/report.log
 
 # Generate all the coverage reports
 method=""
@@ -22,11 +22,13 @@ grep ,triangle.TriangleTest mutation_results/testMap.csv | ( while read -r testL
 done
 echo "testMethods=$method" > foo.properties )
 
-$MAJOR_HOME/bin/ant coverageOriginal
-
-grep ,LIVE mutation_results/killed.csv | cut -f1 -d',' | while read -r mutNo ; do
-    echo $mutNo
-    $MAJOR_HOME/bin/ant coverageMutant -DmutNo=$mutNo
-done
+echo ANALYZING LIVE MUTANTS...
+$MAJOR_HOME/bin/ant coverageOriginal >> mutant_report/report.log
+$MAJOR_HOME/bin/ant coverageMutants >> mutant_report/report.log
+#grep ,LIVE mutation_results/killed.csv | cut -f1 -d',' | while read -r mutNo ; do
+#    printf "$mutNo "
+#    $MAJOR_HOME/bin/ant coverageMutant -DmutNo=$mutNo >> mutant_report/report.log
+#done
+#echo
 
 ./make_report.sh
