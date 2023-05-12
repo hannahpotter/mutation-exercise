5f684c0af157ece43e2f93778c745921379c94ec
Hannah Potter <hkpotter@cs.washington.edu>
2023-05-11 23:16:40 -0700

    Remove extra scripts

--- mutant_info.sh
+++ mutant_info.sh
@@ -2,69 +2,91 @@
 
 MAJOR_HOME="lib/major"
 
-$MAJOR_HOME/bin/ant mutation
-echo Live mutants: $(grep ,LIVE mutation_results/killed.csv | cut -f1 -d',')
+echo RUNNING MUTATION ANALYSIS...
+./mutation.sh > mutant_report/report.log
+grep "Live mutants:" mutant_report/report.log
 
+# Generate all the coverage reports
 method=""
-grep ,$1$ mutation_results/covMap.csv | cut -d ',' -f1 | ( while read -r testNo ; do
-    testName=$(grep ^$testNo, mutation_results/testMap.csv | cut -d ',' -f2)
+grep ,triangle.TriangleTest mutation_results/testMap.csv | ( while read -r testLine ; do
+    testNo=$(echo $testLine | cut -d ',' -f1)
+    testName=$(echo $testLine | cut -d ',' -f2)
     len=$(echo $testName | wc -c)
     end=$(expr $len - 2)
+    testMethod=$(echo $testName | cut -c23-$end)
     if [ ! -z "$method" ]
     then 
         method+=","
     fi
         method+=$(echo $testName | cut -c23-$end)
-    echo $method
 done
-echo "methods=$method" > foo.properties )
-echo "mutNo=$1" >> foo.properties
-lineNum=$(grep ^$1: mutation_results/mutants.log | grep -Eo "classify:[0-9]+" | grep -Eo '[0-9]+')
+echo "testMethods=$method" > foo.properties )
 
-$MAJOR_HOME/bin/ant coverage
+echo ANALYZING LIVE MUTANTS...
+$MAJOR_HOME/bin/ant coverageOriginal >> mutant_report/report.log
+$MAJOR_HOME/bin/ant coverageMutants >> mutant_report/report.log
 
-NEW_REPORT="mutant_report/mutant_info.html"
-mkdir mutant_report
+# Make the report
+NEW_REPORT_DIR="mutant_report/coverage"
+HEADER="<html><head><link title=\"Style\" type=\"text/css\" rel=\"stylesheet\" href=\"../../css/main.css\"/></head><body>"
+FOOTER="</body></html>"
+DEF_FILE="mutant_report/js/def.js"
 
-header=$(sed -n 1,10p coverage_original_results/triangle.Triangle.html)
-footer=$(sed -n 102,104p coverage_original_results/triangle.Triangle.html)
+echo "GENERATING REPORT..."
 
-echo $original_table
-echo $header > $NEW_REPORT
-if [ -f ".mutated/mutants/$1/triangle/Triangle.svg" ]
-then
-    cp ".mutated/mutants/$1/triangle/Triangle.svg" "mutant_report/Triangle.svg"
-    echo "<img src=\"Triangle.svg\" />" >> $NEW_REPORT
-fi
-echo "<h3>Test Information</h3>\n" >> $NEW_REPORT
+rm -rf mutant_report/coverage/original
+rm -rf mutant_report/coverage/mutants
+mkdir -p mutant_report/coverage/original
+mkdir -p mutant_report/coverage/mutants
 
-echo "Tests that covered the mutant:\n" >> $NEW_REPORT
-if [ $(grep ,$1$ mutation_results/covMap.csv | wc -l) -eq 0 ]
-then
-    echo "No tests cover the mutant on line $lineNum" >> $NEW_REPORT
-else
-    echo "<ul>\n" >> $NEW_REPORT
-    grep ,$1$ mutation_results/covMap.csv | cut -d ',' -f1 | while read -r testNo ; do
+grep ,triangle.TriangleTest mutation_results/testMap.csv | while read -r testLine ; do
+    testNo=$(echo $testLine | cut -d ',' -f1)
+    testName=$(echo $testLine | cut -d ',' -f2)
+    len=$(echo $testName | wc -c)
+    end=$(expr $len - 2)
+    testMethod=$(echo $testName | cut -c23-$end)
+
+    line=$(echo $testMethod | grep -o . | grep -n ':')
+    len=$(echo $line | wc -c)
+    end=$(expr $len - 3)
+    line=$(echo $line | cut -c1-$end)
+    line=$(expr $line - 1)
+    innerNumber=$(echo $testMethod | cut -c14-$line)
+    coverageTestNo=$(grep "\[${innerNumber}:" coverage_original_results/testMap.csv | cut -d ',' -f1)
+
+    echo $HEADER > mutant_report/coverage/original/${testNo}-triangle.html
+    sed -n 20,100p coverage_original_results/${coverageTestNo}/triangle.Triangle.html >> mutant_report/coverage/original/${testNo}-triangle.html
+    echo $FOOTER >> mutant_report/coverage/original/${testNo}-triangle.html
+done 
+
+echo "const mut_tests = new Map();\n" > $DEF_FILE
+grep ,LIVE mutation_results/killed.csv | cut -f1 -d',' | while read -r mutNo ; do
+    echo "const mut_${mutNo} = [];" >> $DEF_FILE
+    grep ,$mutNo$ mutation_results/covMap.csv | cut -d ',' -f1 | while read -r testNo ; do
         testName=$(grep ^$testNo, mutation_results/testMap.csv | cut -d ',' -f2)
         len=$(echo $testName | wc -c)
         end=$(expr $len - 2)
-        echo "<li>" >> $NEW_REPORT
-        echo $testName | cut -c23-$end >> $NEW_REPORT
-        echo "</li>\n" >> $NEW_REPORT 
-    done
-    echo "</ul>\n" >> $NEW_REPORT
+        testMethod=$(echo $testName | cut -c23-$end)
 
-    echo "<div class=\"panes\" style=\"display:flex;\">" >> $NEW_REPORT
-    sed -n 20,100p coverage_original_results/triangle.Triangle.html >> $NEW_REPORT
-    sed -n 20,100p coverage_mutant_results/triangle.Triangle.html >> $NEW_REPORT
-    echo "</div>" >> $NEW_REPORT
-fi
-echo $footer >> $NEW_REPORT
+        line=$(echo $testMethod | grep -o . | grep -n ':')
+        len=$(echo $line | wc -c)
+        end=$(expr $len - 3)
+        line=$(echo $line | cut -c1-$end)
+        line=$(expr $line - 1)
+        innerNumber=$(echo $testMethod | cut -c14-$line)
+        coverageTestNo=$(grep "\[${innerNumber}:" .mutated/mutants/${mutNo}/coverage_results/testMap.csv | cut -d ',' -f1)
 
-python3 pretty.py $lineNum
+        echo $HEADER > mutant_report/coverage/mutants/${mutNo}-${testNo}-triangle.html
+        sed -n 20,100p .mutated/mutants/${mutNo}/coverage_results/${coverageTestNo}/triangle.Triangle.html >> mutant_report/coverage/mutants/${mutNo}-${testNo}-triangle.html
+        echo $FOOTER >> mutant_report/coverage/mutants/${mutNo}-${testNo}-triangle.html
 
-mkdir mutant_report/css
-mkdir mutant_report/js
-cp -r coverage_original_results/css mutant_report
-cp -r coverage_original_results/js mutant_report
+        lineNum=$(grep ^$mutNo: mutation_results/mutants.log | grep -Eo "classify:[0-9]+" | grep -Eo '[0-9]+')
+        python3 pretty.py mutant_report/coverage/mutants/${mutNo}-${testNo}-triangle.html $lineNum
+
+        echo "mut_${mutNo}.push({testNo: ${testNo}, testName: \"${testMethod}\"});" >> $DEF_FILE
+    done 
+    echo "mut_tests.set(${mutNo}, mut_${mutNo});\n" >> $DEF_FILE
+done
+
+echo "DONE: mutant_report/index.html"
 
